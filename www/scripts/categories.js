ab85af4b424933514bedbe3cbabb21c945a2eed5
jmjuan <josem.juangracia@gmail.com>
2025-01-07 13:45:05 +0100

    Añado boton de delete para las categorias

--- www/scripts/categories.js
+++ www/scripts/categories.js
@@ -1,25 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
     const categoryList = document.getElementById("category-list");
   
-    // Función para renderizar las categorías en el DOM
+  
     const drawData = (data) => {
       data.forEach((category) => {
         const li = document.createElement("li");
         li.textContent = category.name;
+
+        const button = document.createElement("button");
+        button.textContent = "Delete";
+        li.appendChild(button);
         categoryList.appendChild(li);
       });
     };
+
   
-    // Solicitud al servidor para obtener las categorías
+  
     fetch("http://localhost:3000/categories")
       .then((res) => res.json())
       .then((data) => drawData(data))
       .catch((error) => console.error("Error al cargar categorías:", error));
 
+  
+  
 
     });
 
-    
+   
+
+    //Lógica añadir categoría
     const addNewCategoryBtn = document.getElementById ("add-category");
     const categoryWindow= document.getElementById ("category-window");
     const closeCategoryWindow = document.getElementById ("close-category-window")
